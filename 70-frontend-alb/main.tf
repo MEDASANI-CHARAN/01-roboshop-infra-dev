3ba7ce4a48e88728c8b44ef1bd9ecc12d4fbc3e0
Medasani Charan <charanchinna646@gmail.com>
2025-07-13 07:58:04 +0530

    terraform

--- 70-frontend-alb/main.tf
+++ 70-frontend-alb/main.tf
@@ -54,7 +54,7 @@ resource "aws_lb_listener" "frontend_alb_https" {
 
 resource "aws_route53_record" "frontend_alb" {
   zone_id = var.zone_id
-  name    = "${var.environment}.${var.zone_name}"
+  name    = "*.${var.zone_name}"
   type    = "A"
   allow_overwrite = true
 
